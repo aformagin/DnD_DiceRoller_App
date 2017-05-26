0e16f03442f021e3ebf5ebb78e5e95061dc8b811
Austin <austin.formagin117@gmail.com>
2017-05-26 09:54:14 -0400

    Renamed the splash class

---
+++ app/src/main/java/com/thirdspare/android/androidsheetsandroller/Dice.java
@@ -0,0 +1,16 @@
+package com.thirdspare.android.androidsheetsandroller;
+
+//This is the dice rolling class for the dice layout. Here will be the rolling functions for each type of die
+
+import android.app.Activity;
+
+//Types of die
+//D4
+//D6
+//D8
+//D10
+//D12
+//D20
+//%
+public class Dice extends Activity{
+}
