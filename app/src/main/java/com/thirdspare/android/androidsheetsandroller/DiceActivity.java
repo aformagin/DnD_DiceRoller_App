7b02f8cd6b601508ebd0597876e647534c4d996c
Austin <austin.formagin117@gmail.com>
2017-05-26 21:17:04 -0400

    Added functionality to dice button

--- app/src/main/java/com/thirdspare/android/androidsheetsandroller/DiceActivity.java
+++ app/src/main/java/com/thirdspare/android/androidsheetsandroller/DiceActivity.java
@@ -3,6 +3,7 @@ package com.thirdspare.android.androidsheetsandroller;
 //This is the dice rolling class for the dice layout. Here will be the rolling functions for each type of die
 
 import android.app.Activity;
+import android.os.Bundle;
 
 //Types of die
 //D4
@@ -12,5 +13,12 @@ import android.app.Activity;
 //D12
 //D20
 //%
-public class Dice extends Activity{
+public class DiceActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dice);
+
+    }
 }
